c4e2b13c599770dc381e4fbce829f5034430ba84
Evan Smith <Evan Smith>
2017-05-02 00:26:33 -0400

    Initial commit

---
+++ src/game/Game.java
@@ -0,0 +1,85 @@
+package game;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferStrategy;
+
+/**
+ * Created by Evan_2 on 2017-04-30.
+ */
+public class Game extends Canvas implements Runnable {
+    private static final long serialVersionUID = 1L;
+
+    public static int WIDTH = 30;
+    public static int HEIGHT = 30;
+    public static int SCALE = 15;
+
+    private Thread thread;
+    private JFrame frame;
+    private boolean running = false;
+
+    public Game() {
+        Dimension size = new Dimension(WIDTH*SCALE, HEIGHT*SCALE);
+        setPreferredSize(size); // in Canvas
+
+        frame = new JFrame();
+    }
+
+    public synchronized void start() {
+        running = true;
+        thread = new Thread(this, "Snake");
+        thread.start();
+    }
+
+    public synchronized void stop() {
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        while (running) {
+            update();
+            render();
+
+        }
+    }
+
+    public void update() {
+
+    }
+
+    public void render() {
+        BufferStrategy bs = getBufferStrategy();
+        if (bs == null) {
+            createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+        // Dis where your graphics stuff go.
+        g.setColor(Color.BLACK);
+        g.fillRect(0,0, getWidth(), getHeight());
+        g.dispose();
+        bs.show();
+
+    }
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.frame.setResizable(false);
+        game.frame.setTitle("Snake");
+        game.frame.add(game);
+        game.frame.pack();
+        game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        game.frame.setLocationRelativeTo(null);
+        game.frame.setVisible(true);
+
+        game.start();
+    }
+}
