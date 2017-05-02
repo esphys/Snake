ee3d95af489278ef7de190fad0bb002da9871153
esphys <esphys@gmail.com>
2017-05-02 01:16:07 -0400

    add method for creating pixel array

--- src/game/graphics/Screen.java
+++ src/game/graphics/Screen.java
@@ -13,4 +13,12 @@ public class Screen {
         this.height = height;
         pixels = new int[width * height];
     }
+
+    public void render() {
+        for(int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                pixels [20+29*width] = 0xff00ff;
+            }
+        }
+    }
 }
