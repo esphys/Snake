26f1c9356784d72cd8c7da7ca916fac88f71d771
esphys <esphys@gmail.com>
2017-05-03 01:00:02 -0400

    add demo pixel and clear screen on update of Game

--- src/game/Game.java
+++ src/game/Game.java
@@ -70,7 +70,7 @@ public class Game extends Canvas implements Runnable {
             createBufferStrategy(3);
             return;
         }
-
+        screen.clear();
         screen.render();
 
         for (int i=0; i < pixels.length; i++) {
